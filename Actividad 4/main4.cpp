b2d4c3ba47bd0714e2249655c110979216a626e1
ruben <orestes1096@hotmail.com>
2019-09-17 19:45:15 -0600

    modificaciones

--- Actividad 4/main4.cpp
+++ Actividad 4/main4.cpp
@@ -3,14 +3,14 @@
 #include <string>
 using namespace std;
 
-float suma=0,prom=0,pob=0,aux=0;
+float suma=0,pob=0;
 int x=0,y=0;
 
 struct county{
     string nombre;
     string capital;
     int habitantes;
-    float promedio_edad;
+    int edad;
 };
 
 struct continente{
@@ -38,37 +38,16 @@ int main(){
 			cin>>continentes[i].pais[k].habitantes;
 			
             cout<< "  .Digite el promedio de la edad de los habitantes del pais: ";
-			cin>>continentes[i].pais[k].promedio_edad;
+			cin>>continentes[i].pais[k].edad;
 			
             suma+=continentes[i].pais[k].habitantes;
         }
         cin.clear();
 		cin.ignore(100000,'\n');
-        cout<<"La suma total por continente "<<continentes[i].nombre<< " es: "<<suma<<endl;
+        cout<<"Poblacion total del continente  es: "<<suma<<endl;
 
 
     }
-    for(int i=0;i<5;i++){
-        cout<<continentes[i].nombre<<endl;
-        for(int k=0;k<5;k++){
-            cout<<continentes[i].pais[k].nombre<<endl;
-            cout<<continentes[i].pais[k].capital<<endl;
-            cout<<continentes[i].pais[k].habitantes<<endl;
-            cout<<continentes[i].pais[k].promedio_edad<<endl;
-            aux + = continentes[i].pais[k].promedio_edad;
-        }
-    for (int i=0;i<5;i++){
-        for (int k=0;k<5;k++)
-            if (continentes[i].pais[k].habitantes>pob){
-                pob=continentes[i].pais[k].habitantes;
-                x=i;
-                y=k;
-            }
-        }
-    }
-    prom=aux/25;
-    cout<<"La capital mas poblada es: "<<continentes[x].pais[y].capital<<pob<<endl;
-    cout<<"El promedio mundial es: "<<prom<<endl;
-    
+      
     return 0;
 }
